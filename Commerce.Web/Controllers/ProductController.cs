d40aa97eb4bb39a479fc1ee97aa47ef5b18a3c5c
Мутик Андрей <amutik@masterdm.ru>
2023-12-18 21:36:59 +0300

    Add Commerce.Web project (layer Presentation)

---
+++ Commerce.Web/Controllers/ProductController.cs
@@ -0,0 +1,62 @@
+using Commerce.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using Model.CommandsServices;
+using Model.Entities;
+using Model.Parameters;
+using Model.Results;
+using System;
+using System.Diagnostics;
+
+namespace Commerce.Web.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public ProductController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("/products/")]
+        public IActionResult Products()
+        {
+            IEnumerable<Product> products = RootComposite.GetAllProductService.Execute(new NullParameter()).Products;
+            return View(model: products);
+        }
+
+        [HttpPost("/products/Create")]
+        public IActionResult Create([FromForm] InsertProductParameter insertProductParameter)
+        {
+            Result result = RootComposite.InsertProductService.Execute(insertProductParameter);
+            string message = "Fail";
+            if (result.Code == 0)
+            {
+                RootComposite.Database.Save();
+                message = "Success";
+            }
+            return RedirectToAction(nameof(Result), new { Message = message });
+        }
+
+        [HttpGet("/products/Create")]
+        public IActionResult Create()
+        {
+            InsertProductParameter insertProductParameter = new() { ProductId = Guid.NewGuid(), Name = "", UnitPrice = "", Description ="" };
+            return View(model: insertProductParameter);
+        }
+
+        [HttpGet("/products/Result")]
+        public IActionResult Result(string Message)
+        {
+            ViewBag.Message = Message;
+            return View();
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Commerce.Web/Program.cs
@@ -0,0 +1,45 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore;
+using Model.Interfaces;
+using Model.Parameters;
+using Model.Results;
+using Model.CommandsServices;
+using Repository.Repositories;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
+
+public static class RootComposite
+{
+    public static readonly IUnitOfWork Database = new UnitOfWork(@"Server=(localdb)\mssqllocaldb;Database=Commerce;Trusted_Connection=True");
+    public static readonly ICommandService<InsertProductParameter, Result> InsertProductService = new InsertProductService(Database.Products);
+    public static readonly ICommandService<NullParameter, GetAllProductsResult> GetAllProductService = new GetAllProductService(Database.Products);
+}
+
